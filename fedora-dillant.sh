59993c522a6fdbc845f4ca4ef9c5094e662eb699
Dillon-The-Geek <113472229+Dillon-The-Geek@users.noreply.github.com>
2023-12-17 22:08:47 +0000

    Add files via upload

--- fedora-dillant.sh
+++ fedora-dillant.sh
@@ -14,4 +14,55 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 
 sudo dnf config-manager --enable fedora-cisco-openh264
 
+#install brave 
 
+
+sudo dnf install dnf-plugins-core
+
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+
+sudo dnf install brave-browser
+
+#install media apps
+
+sudo dnf install discord
+
+sudo dnf install obs-studio
+
+#install flatpak and add flathub 
+
+sudo dnf install flatpak
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo    
+
+#timeshift
+
+sudo dnf install timeshift
+
+#gaming
+
+sudo dnf install steam
+
+sudo dnf install lutris
+
+sudo dnf install mangohud
+
+sudo dnf install goverlay
+
+#shotcut
+
+mkdir AppImages
+
+wget "https://sourceforge.net/projects/shotcut/files/v23.12.15/shotcut-linux-x86_64-231215.AppImage/download"
+
+mv shotcut-linux-x86_64-231215.AppImage ~/AppImages
+
+#neofetch
+
+sudo dnf install neofetch
+
+echo "neofetch" >> .bashrc
+
+reboot
